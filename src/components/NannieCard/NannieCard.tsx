e8207fb9ec4fc108b3c5bde4d3cb2139723e7808
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-06-11 13:21:18 +0300

    add nannie fetch

---
+++ src/components/NannieCard/NannieCard.tsx
@@ -0,0 +1,131 @@
+import css from "./NannieCard.module.scss";
+import sprite from "../../img/sprite.svg";
+
+interface Review {
+  comment: string;
+  rating: number;
+  reviewer: string;
+}
+
+interface Nannie {
+  about: string;
+  avatar_url: string;
+  birthday: string;
+  characters: string;
+  education: string;
+  experience: string;
+  kids_age: string;
+  location: string;
+  name: string;
+  price_per_hour: number;
+  rating: number;
+  reviews: Review[];
+}
+
+interface Props {
+  nanny: Nannie;
+  showMore: number | null;
+  toggleReadMore: (index: number) => void;
+}
+
+const NannieCard: React.FC<Props> = ({ nanny, showMore, toggleReadMore }) => {
+  const index = nanny.name.length; // 👈 временно, либо передавай `index` как проп
+
+  return (
+    <li className={css.teachersContainerItem}>
+      <div className={css.teachersItem}>
+        <div className={css.photoWrapper}>
+          <img
+            className={css.teacherImg}
+            src={nanny.avatar_url}
+            alt={nanny.name}
+          />
+          <span className={css.status}>
+            <svg width={12} height={12}>
+              <use href={`${sprite}#online`}></use>
+            </svg>
+          </span>
+        </div>
+
+        <div className={css.descWrapper}>
+          <div className={css.teacherDesc}>
+            <div className={css.teacherName}>
+              <p>About nanny</p>
+              <h3>{nanny.name}</h3>
+            </div>
+
+            <div className={css.teacherAchieve}>
+              <p className={css.flexGap}>
+                <span>
+                  <svg width={16} height={16}>
+                    <use href={`${sprite}#star`}></use>
+                  </svg>
+                </span>
+                Rating: {nanny.rating}
+              </p>
+              <span className={css.line}></span>
+              <p>
+                Price / 1 hour:{" "}
+                <span className={css.priceColor}>{nanny.price_per_hour}$</span>
+              </p>
+            </div>
+          </div>
+
+          <div className={css.teacherInfo}>
+            <div className={css.teacherInfoContainer}>
+              <span className={css.spanWord}>Education:</span>
+              <p className={css.infoWrapper}>{nanny.education}</p>
+            </div>
+
+            <div className={css.teacherInfoContainer}>
+              <span className={css.spanWord}>Experience:</span>
+              <p className={css.infoWrapper}>{nanny.experience}</p>
+            </div>
+
+            <div className={css.teacherInfoContainer}>
+              <span className={css.spanWord}>Location:</span>
+              <p className={css.infoWrapper}>{nanny.location}</p>
+            </div>
+
+            {showMore !== index && (
+              <button
+                className={css.readMoreBtn}
+                onClick={() => toggleReadMore(index)}
+              >
+                Read more
+              </button>
+            )}
+
+            {showMore === index && (
+              <div className={css.moreInfo}>
+                <p className={css.moreInfoDesc}>{nanny.about}</p>
+                <ul className={css.moreInfoList}>
+                  {nanny.reviews.map((review, idx) => (
+                    <li key={idx}>
+                      <div className={css.moreInfoItem}>
+                        <div>
+                          <span className={css.spanWord}>
+                            {review.reviewer}
+                          </span>
+                          <p className={css.infoWrapper}>
+                            <svg width={16} height={16}>
+                              <use href={`${sprite}#star`}></use>
+                            </svg>{" "}
+                            {review.rating}.0
+                          </p>
+                        </div>
+                      </div>
+                      <p className={css.infoWrapper}>{review.comment}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default NannieCard;
--- src/pages/NanniesPage/NanniesPage.tsx
+++ src/pages/NanniesPage/NanniesPage.tsx
@@ -1,12 +1,64 @@
 import css from "./NanniesPage.module.scss";
 import Header from "../../components/Header/Header";
 import Filters from "../../components/Filters/Filters";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../../api/axiosInstance";
+import NannieCard from "../../components/NannieCard/NannieCard";
+
+interface Review {
+  comment: string;
+  rating: number;
+  reviewer: string;
+}
+
+interface Nannie {
+  about: string;
+  avatar_url: string;
+  birthday: string;
+  characters: string;
+  education: string;
+  experience: string;
+  kids_age: string;
+  location: string;
+  name: string;
+  price_per_hour: number;
+  rating: number;
+  reviews: Review[];
+}
 
 const NanniesPage = () => {
+  const [nannies, setNannies] = useState<Nannie[]>([]);
+  const [showMore, setShowMore] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchNannies = async () => {
+      try {
+        const response = await axiosInstance.get("/nannies.json");
+        setNannies(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchNannies();
+  }, []);
+
+  const toggleReadMore = (index: number) => {
+    setShowMore((prev) => (prev === index ? null : index));
+  };
+
   return (
     <section className={css.nannies}>
       <Header fullWidth theme="white" />
       <Filters />
+
+      {nannies.map((nanny) => (
+        <NannieCard
+          key={nanny.name}
+          nanny={nanny}
+          showMore={showMore}
+          toggleReadMore={toggleReadMore}
+        />
+      ))}
     </section>
   );
 };
